01f1c8792b96d17c7d0d83a6171209c408bad532
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-03-21 23:10:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('userDataForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    openPopup();
+  });
+
+function getPopup(){
+    window.alert("You are succcessfully logged in")
+}
+  
